e50b7d0a1b751dc5eafab6a5a547faae8e03e85e
kokilaeie <srikokila27@gmail.com>
2016-08-09 17:21:16 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,18 @@
+class fibonacci
+{
+public static void main(String []args)
+{
+Scanner s=new Scanner(System.in);
+int a[]=new int[n];
+b[0]=0;
+b[1]=1;
+for(int i=0;i<n;i++)
+{
+b[i]=b[i-1]+b[i-2];
+}
+for(int i=0;i<n;i++)
+{
+System.out.println(b[i]" ");
+}
+}
+}
